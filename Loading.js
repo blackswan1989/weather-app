f13e9b3d760713068a022a4729c257fbfb04f28f
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-11-10 11:18:03 +0900

    Add LinearGradient & icon

--- Loading.js
+++ Loading.js
@@ -1,25 +1,49 @@
 import React from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { Feather } from "@expo/vector-icons";
+
+const loadingPage = {
+  gradient: ["#f7797d", "#FBD786", "#C6FFDD"],
+  iconName: "loader",
+};
 
 export default function Loading() {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" />
-      <Text style={styles.text}>Bring the weather...🎅🏼</Text>
-    </View>
+    <LinearGradient colors={loadingPage.gradient} style={styles.container}>
+      <View style={styles.containerTop}>
+        <Feather name={loadingPage.iconName} size={120} color="white" />
+      </View>
+      <View style={styles.containerBottom}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.text}>Bring the weather...🎅🏼</Text>
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-end",
-    paddingHorizontal: 30,
-    paddingVertical: 150,
-    backgroundColor: "#55967e",
+    justifyContent: "center",
+    alignItems: "center",
+    // justifyContent: "flex-end",
+    // paddingHorizontal: 30,
+    // paddingVertical: 150,
+    // backgroundColor: "#006E33",
+  },
+  containerTop: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  containerBottom: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
-    color: "#c72e45",
+    color: "white",
     fontSize: 28,
     fontWeight: "700",
   },
